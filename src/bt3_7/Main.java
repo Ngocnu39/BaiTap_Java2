fbb51916c9f5af8e82049953ac449be73dd895ee
Ngocnu39 <nuctn.24ite@vku.udn.vn>
2025-02-21 00:07:49 +0700

    bt

---
+++ src/bt3_7/Main.java
@@ -0,0 +1,17 @@
+package bt3_7;
+
+public class Main {
+    public static void main(String[] args) {
+        Ball ball = new Ball(50, 50, 5, 2, 3);
+        Player player = new Player(7, 40, 40, 20, 20);
+
+        for (int i = 0; i < 10; i++) {
+            ball.move();
+            if (player.collidesWith(ball)) {
+                ball.reflectHorizontal();
+                ball.reflectVertical();
+            }
+            System.out.println(ball);
+        }
+    }
+}
---
+++ src/bt4_6/Animal.java
@@ -0,0 +1,14 @@
+package bt4_6;
+
+public class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal[name=" + name + "]";
+    }
+}
